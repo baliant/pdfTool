5ebdf7285d02f1059f94ddec9bd14a5990f0c533
baliant <39745297+baliant@users.noreply.github.com>
2025-10-07 14:22:15 +0200

    Update pdftool.py

--- pdftool.py
+++ pdftool.py
@@ -14,7 +14,7 @@ except Exception:
     _HAVE_YAML = False
 
 
-icon_url = 📚
+icon_url = "https://raw.githubusercontent.com/baliant/pdftool/main/icon/koru_logo.png"
 
 st.set_page_config(
     page_title="PDF Select & Merge",
